c68865f9e90c1d8c5b3b8cb856bd0dff3825d8a6
Nashika <nashimeher@gmail.com>
2024-01-20 16:27:21 +0530

    optimised mobile menu-bar

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,9 +1,10 @@
-const mobileMenuBtnElement = document.getElementById('mobile-menu-bar');
+const mobileMenuBtnElement = document.querySelector('.toggle');
 const mobileMenuPage = document.getElementById('mobile-menu');
 const menuPageListElements = document.querySelectorAll('#mobile-menu .nav-items li');
 
 function toggleMobileMenu () {
     mobileMenuPage.classList.toggle('active')
+    mobileMenuBtnElement.classList.toggle('active')
 
     for(const menuPageListElement of menuPageListElements){
         menuPageListElement.classList.toggle('active')
