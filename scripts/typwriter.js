f027ba173c531fce66defc3ef41597ffac6b119d
Nashika <nashimeher@gmail.com>
2024-01-23 16:25:11 +0530

    optimised typreWriter animation

--- scripts/typwriter.js
+++ scripts/typwriter.js
@@ -23,7 +23,7 @@ var TypeWriter = new Typewriter(aboutContent,{
 });
 
 TypeWriter
-.pauseFor(1000)
+.pauseFor(2000)
 .typeString(`<p >From engaging UIs to <span>robust functionality</span>, I render websites that are meticulously designed to <span>elevate your brand</span> and drive success in the digital world.</p>`)
 .typeString(`<p> Weather you're  a <span>small business</span>, an <span>entrepreneur</span> or a <span>content creator</span>, I will enhance your <span>online presence.</span> I pay attention to every detail and commitment into building user-friendly experiences.</p>`)
 .typeString(`<p"> I make sure to deliver the projects on time and within the <span>budget</span>. My strong communication skills ensure <span>smooth collaboration</span>.</p>`)
