ac9735faf89af41c29ac08fce4bea9a46499c43a
Nashika <nashimeher@gmail.com>
2024-01-23 16:13:15 +0530

    optimised typreWriter animation

--- scripts/typwriter.js
+++ scripts/typwriter.js
@@ -1,10 +1,3 @@
-/* <h1 class="title hidden stager">Hey! I'm Nashika, a full-stack web developer</h1>
-<p class="hidden stager"> I occasionally take on freelancing opportunities and build aesthetic front-end and back-end projects</p> */
-
-{/* <p class="hidden stager">From engaging UIs to <span>robust functionality</span>, I render websites that are meticulously designed to <span>elevate your brand</span> and drive success in the digital world. Weather you're  a <span>small business</span>, an <span>entrepreneur</span> or a <span>content creator</span>, I will enhance your <span>online presence.</span> I pay attention to every detail and commitment into building user-friendly experiences.</p>
-<p class="hidden stager"> I make sure to deliver the projects on time and within the <span>budget</span>. My strong communication skills ensure <span>smooth collaboration</span>.</p>
-<a href="https://on.contra.com/kL7WB8">Know more <span> on.contra</span></a> */}
-
 var landingPageTitle = document.querySelector('#landing-page .content span');
 var TypeWriter = new Typewriter(landingPageTitle,{
     delay: 75,
@@ -30,6 +23,7 @@ var TypeWriter = new Typewriter(aboutContent,{
 });
 
 TypeWriter
+.pauseFor(1000)
 .typeString(`<p >From engaging UIs to <span>robust functionality</span>, I render websites that are meticulously designed to <span>elevate your brand</span> and drive success in the digital world.</p>`)
 .typeString(`<p> Weather you're  a <span>small business</span>, an <span>entrepreneur</span> or a <span>content creator</span>, I will enhance your <span>online presence.</span> I pay attention to every detail and commitment into building user-friendly experiences.</p>`)
 .typeString(`<p"> I make sure to deliver the projects on time and within the <span>budget</span>. My strong communication skills ensure <span>smooth collaboration</span>.</p>`)
