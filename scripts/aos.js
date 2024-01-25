4c6da481e36621623254a11e64179da4eb055403
Nashika <nashimeher@gmail.com>
2024-01-25 19:22:34 +0530

    optimised scroll animations

--- scripts/aos.js
+++ scripts/aos.js
@@ -18,14 +18,32 @@ const observerAlt = new IntersectionObserver((entries) => {
     for(const entry of entries){
         console.log(entry)
         if (entry.isIntersecting){
-            entry.target.classList.add('show-alt');
+            entry.target.classList.add('slide-in');
         }else {
-            entry.target.classList.remove('show-alt')
+            entry.target.classList.remove('slide-in')
         }
     }
 });
 
-const hiddenAltElements = document.querySelectorAll('.hidden-alt');
+const hiddenAltElements = document.querySelectorAll('.slide-out');
 for (const hiddenAltElement of hiddenAltElements){
     observerAlt.observe(hiddenAltElement);
 }
+
+
+const observerImg = new IntersectionObserver((entries) => {
+    for(const entry of entries){
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('scale-in');
+        }else {
+            entry.target.classList.remove('scale-in')
+        }
+    }
+});
+
+const hiddenImgElements = document.querySelectorAll('.scale-out');
+for (const hiddenImgElement of hiddenImgElements){
+    observerImg.observe(hiddenImgElement);
+}
+
